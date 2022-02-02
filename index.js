3e98a2a2ea7f4da30ac1770c473afc2c10a3f305
Dagmawi Babi <44819176+dagmawibabi@users.noreply.github.com>
2022-02-02 19:04:48 +0300

    message

---
+++ index.js
@@ -0,0 +1,22 @@
+let express = require("express");
+let app = express();
+
+// Home
+app.get("/",(req, res) => {
+    res.send("Welcome to New-Social API");
+});
+
+
+// Home
+app.get("/",(req, res) => {
+    res.send("Welcome to New-Social API");
+});
+
+
+
+
+let pornNum = process.env.PORT || 7000;
+app.listen(pornNum, ()=>{
+    console.log(`Server listening on port ${portNum}`);
+})
+
