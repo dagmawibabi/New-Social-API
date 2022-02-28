6ff4bec1acbbbda553102f4c62b4f326279a71cd
Dagmawi Babi <44819176+dagmawibabi@users.noreply.github.com>
2022-03-01 00:35:30 +0300

    added more sample messages with gifs

--- index.js
+++ index.js
@@ -18,30 +18,42 @@ app.get("/api/",(req, res) => {
 // G L O B A L  C H A T
 let globalMessages = [
     {
+        "dp": "https://i.pinimg.com/564x/f8/9b/97/f89b97b7b61ef45ca3025c7a0bbd9310.jpg",
         "sender": "BoJack",
         "message": "Well this is depressing",
         "date": "12/2/2022",
         "time": "4:22:14 PM",
     },
     {
+        "dp": "https://i.pinimg.com/564x/94/29/8a/94298a0acf7be013ec747aad82b5da13.jpg",
         "sender": "Nate",
         "message": "This's cool",
         "date": "12/2/2022",
         "time": "4:30:16 PM",
     },
     {
+        "dp": "https://i.pinimg.com/564x/bf/e5/fd/bfe5fd63c5124fbb3730c5b9e2d3bc01.jpg",
         "sender": "Batman",
         "message": "I'm Batman 🦇",
         "date": "12/2/2022",
         "time": "4:46:12 PM",
     },
+    {
+        "dp": "https://i.pinimg.com/originals/62/82/0a/62820a3251082ac356b5fd45c8cc324f.gif",
+        "sender": "BlueRed",
+        "message": "This is awesome design",
+        "date": "12/2/2022",
+        "time": "4:52:42 PM",
+    },
+    
 ];
 
 // Send Global Message
-app.get("/api/sendGlobalMessage/:sender/:message",(req, res)=>{
+app.get("/api/sendGlobalMessage/:sender/:message/:dp",(req, res)=>{
     const timeElapsed = Date.now();
     const today = new Date(timeElapsed);    
     let messageObject = {
+        "dp": (req.params.sender != "") ? req.params.sender : "",
         "sender": req.params.sender,
         "message": req.params.message,
         "date": today.toLocaleDateString(),
