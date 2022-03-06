ccecb3f1cf33452977399e6cd6c94709aa061928
Dagmawi Babi <44819176+dagmawibabi@users.noreply.github.com>
2022-03-06 18:50:14 +0300

    send and receive from db made

--- index.js
+++ index.js
@@ -93,7 +93,7 @@ app.get("/api/sendGlobalMessage/:sender/:message",(req, res)=>{
 app.get("/api/receiveGlobalMessage", (req, res)=>{
     // fetch from a db
     let globalMessagesFromDB = [];
-    messagesModel.find().then((result) => {globalMessagesFromDB = result;});
+    await messagesModel.find().then((result) => {globalMessagesFromDB = result;});
     for(messages of globalMessagesFromDB){
         globalMessages.push(messages);
     }
